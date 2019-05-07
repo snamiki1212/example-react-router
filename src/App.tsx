a35a4c42a90f7297a925e3363b76c57a9c6f04bc
snamiki1212 <snamiki1212@gmail.com>
2019-05-07 15:22:13 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route, Redirect} from 'react-router';
+import Home from './components/Home';
+import About from './components/About';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/" component={Home} />
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
